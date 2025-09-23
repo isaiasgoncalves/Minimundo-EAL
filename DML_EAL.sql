70d07c4b939598b0f9bfd6f86a203daebc49f955
José Thevez Gomes Guedes <162639354+ThevezGomes@users.noreply.github.com>
2025-09-23 13:08:33 -0300

    Corrigindo nome dos esquemas de EAL para oper_eal

--- DML_EAL.sql
+++ DML_EAL.sql
@@ -165,7 +165,7 @@ INSERT INTO oper_eal.VeiculoAula (AulaID, IDVeiculo, DtHrInicio, DtHrFim) VALUES
 (5010, 1, '2025-09-05 16:00:00', NULL);
 
 
-INSERT INTO EAL.Exame (ExameID, Status, Nota, DtHrIni, DtHrFim, AlunoID, FuncID) VALUES
+INSERT INTO oper_eal.Exame (ExameID, Status, Nota, DtHrIni, DtHrFim, AlunoID, FuncID) VALUES
 (201, 'Aprovado', 85.0, '2025-09-15 10:00:00', '2025-09-15 11:00:00', 101, 7),
 (202, 'Reprovado', 45.5, '2025-09-16 14:00:00', '2025-09-16 15:00:00', 102, 8),
 (203, 'Agendado', NULL, '2025-09-22 09:00:00', '2025-09-22 10:00:00', 103, 7),
@@ -180,12 +180,12 @@ INSERT INTO oper_eal.ExamePratico (ExameID, IDVeiculo) VALUES
 (203, 1);
 
 INSERT INTO oper_eal.Pagamento (PagtoID, PagtoValor, PagtoData, FuncID) VALUES
-INSERT INTO EAL.ExameTeorica (ExameID, IDSala) VALUES
+INSERT INTO oper_eal.ExameTeorica (ExameID, IDSala) VALUES
 (204, 1),
 (205, 1),
 (206, 2);
 
-INSERT INTO EAL.Pagamento (PagtoID, PagtoValor, PagtoData, FuncID) VALUES
+INSERT INTO oper_eal.Pagamento (PagtoID, PagtoValor, PagtoData, FuncID) VALUES
 (501, 160.00, '2025-08-30', 9),
 (502, 450.00, '2025-08-31', 3),
 (503, 750.00, '2025-09-01', 9),
