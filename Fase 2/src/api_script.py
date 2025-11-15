169aed04c550cab252235a50eb6e465f6facaddf
Isaías Gouvêa Gonçalves <isaias.ggoncalves@gmail.com>
2025-11-15 16:28:14 -0300

    organização

---
+++ Fase 2/src/api_script.py
@@ -0,0 +1,22 @@
+import anthropic
+import pandas as pd
+import os
+import prompt as pt
+
+"""
+Vou organizar agora o esqueleto do nosso script, com o pipeline geral
+"""
+
+def main():
+
+    df = pd.read_csv('arquivo.csv')
+
+    print(df)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
